e1e69cf9441e375eeb81e2f0d57aa97bd0b77776
Sauka <kudzysauka@gmail.com>
2022-12-11 06:54:16 +0900

    working on try.ipynb
    Rough work on jupyter notebook.

--- src/Attributes.py
+++ src/Attributes.py
@@ -1,8 +1,17 @@
+from test2 import result
+from test2 import arr
+import pandas as pd
 class Attributes:
     # desinations list
-    addressList = []
-    # 2D Adjacent matrix
-    apiResult = []
+    addressList = arr
+    # 3D Adjacent matrix
+    apiResult = result
     #Result path(after algorithm)
     path = []
     
+
+    df=pd.Series(result)
+    print(apiResult)
+    print(f"results{apiResult[1]}")
+    print(f"addressList{arr}")
+    df.to_pickle("./dummy.pkl")  
